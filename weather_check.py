c23fa2ec91be05e1fe0930c6a69324fb76a090cd
Dreyphus <dvd.cooney@gmail.com>
2024-11-12 08:16:54 +0900

    added historic date range selection. change variable names

--- weather_check.py
+++ weather_check.py
@@ -23,27 +23,27 @@ import statistics
 
 
 def make_date(y, m, d):
-    year = int(y)
-    month = int(m)
-    day = int(d)
+    _year = int(y)
+    _month = int(m)
+    _day = int(d)
 
-    date = datetime(year, month, day)
-    return date
+    tar_date = datetime(_year, _month, _day)
+    return tar_date
 
 def find_city(long, lat):
     city = Point(long, lat)
     return city
     
-def get_today_temperature(long, lat, date):
-    
-    city = Point(long, lat)
-    data = Daily(city, date, date)
-    data = data.fetch()
+def get_today_temperature(long, lat, tar_date):
+
+    _city = Point(long, lat)
+    _data = Daily(_city, tar_date, tar_date)
+    _data = _data.fetch()
 
     # pull today's temp data and print it
-    if not data.empty:
-        temp_today = data['tavg'].iloc[0]    
-        #print(f"Today's Temperature {temp_today}°C")
+    if not _data.empty:
+        temp_today = _data['tavg'].iloc[0]    
+        #print(f"Today's Temperature {temp_today}C")
         return temp_today
     else:
         return None
@@ -51,23 +51,24 @@ def get_today_temperature(long, lat, date):
         #return None
 
 # pull historical data and print it
-def get_historical_temperatures(long, lat, date):
-    #generate dates for the last 20 years
-    city = Point(long, lat)
-    years_back = 20
-    historical_dates = [date - timedelta(days=365 * i) for i in range(0, years_back + 1)]
+def get_historical_temperatures(lat, long, in_date, tar_range):
+    #generate dates for the range in years
+    _city = Point(lat, long)
+    years_back = int(tar_range)
+    years_back+=1
+    historical_dates = [in_date - timedelta(days=365 * i) for i in range(0, years_back)]
     
     historical_data = []
     
-    for date in historical_dates:
-        data = Daily(city, date, date)
-        data = data.fetch()
+    for _date in historical_dates:
+        _data = Daily(_city, _date, _date)
+        _data = _data.fetch()
         
-        if not data.empty:
-            temp = data['tavg'].iloc[0]
-            historical_data.append((date.date(), temp))
+        if not _data.empty:
+            _temp = _data['tavg'].iloc[0]
+            historical_data.append((_date.date(), _temp))
         else:
-            historical_data.append((date.date(), 'No data'))
+            historical_data.append((_date.date(), 'No data'))
             #print(f"No data available for {date.date()}")
     
     return historical_data
--- weather_flask.py
+++ weather_flask.py
@@ -5,8 +5,8 @@ import dash
 from dash import dcc, html
 import plotly.graph_objects as go
 
-long_lat = {'longitute': [53.3498, 51.5072, 48.8575, 35.6764, 40.7128, 28.6139, 25.2048, 39.9042],
-            'latitude': [6.2603, 0.1276, 2.3514, 139.6500, 74.0060, 77.2088, 55.2708, 116.4074]}
+long_lat = {'latitude': [53.3498, 51.5072, 48.8575, 35.6764, 40.7128, 28.6139, 25.2048, 39.9042],
+            'longitude': [6.2603, 0.1276, 2.3514, 139.6500, 74.0060, 77.2088, 55.2708, 116.4074]}
 
 cities_df = pd.DataFrame(long_lat, index=['dublin', 'london', 'paris', 'tokyo', 'new york', 'new delhi', 'dubai', 'beijing'])
 
@@ -23,21 +23,22 @@ def home():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    # gather input
     year_input = request.form['user_input']
     month_input = request.form['month']
     day_input = request.form['day']
-
     city_check = request.form['city']
+    range_input = request.form['range']
 
-    # checks the data fram of cities for the city name provided   
-    long = cities_df.loc[city_check]['longitute'] 
-    lat = cities_df.loc[city_check]['latitude']
+    # retrieve longitute and latitude for city name   
+    _lat = cities_df.loc[city_check]['latitude'] 
+    _long = cities_df.loc[city_check]['longitude']
     # gets the date
-    date = weather_check.make_date(year_input, month_input, day_input)
+    tar_date = weather_check.make_date(year_input, month_input, day_input)
 
-    today_temp = weather_check.get_today_temperature(long, lat, date)
+    today_temp = weather_check.get_today_temperature(_lat, _long, tar_date)
     global historical_temp
-    historical_temp = weather_check.get_historical_temperatures(long, lat, date)
+    historical_temp = weather_check.get_historical_temperatures(_lat, _long, tar_date, range_input)
     
     return render_template('result.html', result=today_temp, hist_result=historical_temp)
 
@@ -50,11 +51,11 @@ def update_graph(_):
     x_values = [point[0] for point in historical_temp]
     y_values = [point[1] for point in historical_temp]
 
-    fig = go.Figure(
+    _fig = go.Figure(
         data=[go.Scatter(x=x_values, y=y_values, mode='markers+lines', marker=dict(size=8, color='blue'))],
         layout=go.Layout(title="Average temperatures over the selected period", xaxis_title="Year", yaxis_title="Temperatures")
     )
-    return fig
+    return _fig
 
 dash_app.layout = html.Div([
     dcc.Graph(id='temp_plot'),
